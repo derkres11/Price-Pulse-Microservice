0dc140b0b4a5e094d3e88d4568fadb2cc772fff8
Denis Sadovskii <sadovskidg@mail.ru>
2026-02-15 03:14:57 +0300

    Product service created

---
+++ internal/service/product_service.go
@@ -0,0 +1,69 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"pricepulse/internal/domain"
+
+	"github.com/derkres11/price-pulse/internal/domain"
+)
+
+type ProductService struct {
+	repo domain.ProductRepository
+}
+
+func NewProductService(repo domain.ProductRepository) *ProductService {
+	return &ProductService{repo: repo}
+}
+
+
+func (s *ProductService) TrackProduct(ctx context.context, url string, target_price float64) error {
+	p := &domain.Product{
+		URL: url,
+		TargetPrice: target_price,
+		Title: "Pending...",
+	}
+
+	return s.repo.Create(ctx, p)
+}
+
+func (s *ProductService) CheckPrices(ctx context.Context) error {
+	products, err := s.repo.GetAll(ctx)
+	if err != nil {
+		return fmt.Errorf("error fetching products: %w", err)
+	}
+
+	for _, p := range products {
+
+		newprice := s.mockFetchPrice(p.URL){
+			continue
+		}
+
+		if newPrice == p.CurrentPrice {
+			continue
+		}
+
+		err := s.repo.UpdatePrice(ctx, p.ID, newPrice)
+		if err != nil {
+			log.Printf("error updating price for product %d: %v", p.ID, err)
+			continue
+		}
+
+		if newPrice <= p.TargetPrice {
+			log.Printf("Price alert for product %d! Current price: %.2f, Target price: %.2f", p.ID, newPrice, p.TargetPrice)
+		}
+
+	}
+
+	return nil
+
+
+}
+
+func (s *ProductService) mockFetchPrice(url string) (float64, error) {
+	// This is a stub. In a real implementation, you'd fetch the page and parse the price.
+	// For now, let's just return a random price for demonstration purposes.
+	return 99.99, nil
+}
+
