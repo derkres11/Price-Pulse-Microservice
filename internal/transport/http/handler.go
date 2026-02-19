babb93ab5124ab4b65e19c2f60742764c55eb680
Denis Sadovskii <sadovskidg@mail.ru>
2026-02-19 06:23:59 +0300

    Swagger documentation created

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -5,10 +5,12 @@ import (
 	"net/http"
 	"strconv"
 
+	_ "github.com/derkres11/price-pulse/docs" // Import generated docs
 	"github.com/derkres11/price-pulse/internal/domain"
 	"github.com/derkres11/price-pulse/internal/service"
 	"github.com/gin-gonic/gin"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/swaggo/gin-swagger"
 )
 
 type Handler struct {
@@ -16,13 +18,18 @@ type Handler struct {
 	logger   *slog.Logger
 }
 
-// Update constructor to accept logger
 func NewHandler(services *service.ProductService, logger *slog.Logger) *Handler {
 	return &Handler{
 		services: services,
 		logger:   logger,
 	}
 }
+
+// @title PricePulse API
+// @version 1.0
+// @description API Server for Price Monitoring Service
+// @host localhost:8080
+// @BasePath /
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.Default()
@@ -39,7 +46,16 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	return router
 }
 
-// CreateProduct handler
+// CreateProduct godoc
+// @Summary Create a new product
+// @Tags products
+// @Accept json
+// @Produce json
+// @Param input body domain.Product true "Product info"
+// @Success 201 {object} domain.Product
+// @Failure 400 {object} map[string]string
+// @Router /products [post]
+
 func (h *Handler) CreateProduct(c *gin.Context) {
 	var input domain.Product
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -58,7 +74,15 @@ func (h *Handler) CreateProduct(c *gin.Context) {
 	c.JSON(http.StatusCreated, input)
 }
 
-// GetProduct handler
+// GetProduct godoc
+// @Summary Get product by ID
+// @Tags products
+// @Produce json
+// @Param id path int true "Product ID"
+// @Success 200 {object} domain.Product
+// @Failure 404 {object} map[string]string
+// @Router /products/{id} [get]
+
 func (h *Handler) GetProduct(c *gin.Context) {
 	idStr := c.Param("id")
 	id, err := strconv.ParseInt(idStr, 10, 64)
